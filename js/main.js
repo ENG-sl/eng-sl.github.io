bac2f06302a38f20f85e67f5427fd00d16349a9c
DEXOW <51286591+DEXOW@users.noreply.github.com>
2021-06-12 12:11:19 +0530

    Website 1.1 (Mobile View)

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// if ($(window).width() < 1500) {
+//     $('#nav-bar .navbar-nav').removeClass('navbar-center');
+// } else {
+//     $('#nav-bar .navbar-nav').addClass('navbar-center');
+// }
+
+// $(window).resize(function() {
+//     if ($(window).width() < 1300) {
+//         $('.navigation').removeClass('navbar-center');
+//     } else {
+//         $('.workContainer').addClass('pink-colors');
+//     }
+// }).resize(); // trigger resize event initially
+
+// jQuery(document).ready(function($) {
+//     var alterClass = function() {
+//       var ww = document.body.clientWidth;
+//       if (ww < 1300) {
+//         $('.navbar-nav').removeClass('navbar-center');
+//       }
+//     };
+//     $(window).resize(function(){
+//       alterClass();
+//     });
+//     //Fire it when the page first loads:
+//     alterClass();
+//   });
